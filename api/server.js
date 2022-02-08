0cf225ed1a589562568b3d265ec4c0b2a0a4eebd
bugsbrog <bugsbrog@gmail.com>
2022-02-08 12:27:25 -0700

    Done with server.js

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+const postsRouter = require('./posts/posts-router')
+
+server.use(express.json())
+
+server.use('/api/posts', postsRouter)
+
+server.use('*', (req, res) => {
+    res.status(404).send(`
+    <p>Having trouble finding that resource</p>
+    `)
+})
+
+module.exports = server
