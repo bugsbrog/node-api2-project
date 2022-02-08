9308c2ff58a22fd951aedc675b197058beb6db16
bugsbrog <bugsbrog@gmail.com>
2022-02-08 12:38:27 -0700

    Everything set up to start working on posts-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,55 @@
-// implement your posts router here
+const Post = require('./posts-model')
+const router = require('express').Router()
+
+router.get('/', async (req, res) => {
+    try {
+
+    } catch {
+
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+
+    } catch {
+
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch {
+
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+        try {
+
+        } catch {
+
+        }
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+        try {
+
+        } catch {
+
+        }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    const { id } = req.params
+        try {
+
+        } catch {
+
+        }
+})
+
+module.exports = router
