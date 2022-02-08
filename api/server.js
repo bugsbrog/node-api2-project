5bc5bc4438cf114a5b387315fe9ff550907da5c3
bugsbrog <bugsbrog@gmail.com>
2022-02-08 14:29:11 -0700

    Changed error message on server.js

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.use('/api/posts', postsRouter)
 
 server.use('*', (req, res) => {
     res.status(404).json({
-        message: 'Having trouble finding that resource'
+        message: 'Having trouble finding this'
     })
 })
 
