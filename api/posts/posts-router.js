814daef56a320add99fa2bffa45905276a342ed9
bugsbrog <bugsbrog@gmail.com>
2022-02-08 13:43:56 -0700

    create(post) done and working. Kinda lost tho

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -31,13 +31,23 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    try {
-
-    } catch {
-        res.status(500).json({
-            message: 'There was an error while saving the post to the database'
-        })
-    }
+    const { title, contents } = req.body
+        try {
+            if (!title || !contents) {
+                res.status(400).json({
+                    message: 'Please provide title and contents for the post'
+                })
+            } else {
+                const createPost = await Posts.insert({ title, contents })
+                // WHY DO WE HAVE TO DO IT THIS WAY?
+                const newPost = await Posts.findById(createPost.id)
+                res.status(201).json(newPost)
+            }
+        } catch {
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database'
+            })
+        }
 })
 
 router.put('/:id', async (req, res) => {
